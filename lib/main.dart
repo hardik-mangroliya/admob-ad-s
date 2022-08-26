35f9bfd4d1c2cad66336eb2f527dee5ac9f561c3
hardik mangroliya <hardik.mangroliya@joflee.com>
2022-08-26 17:52:07 +0530

    duration added.

--- lib/main.dart
+++ lib/main.dart
@@ -2,52 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/module/view/screen/banner_ad_screen.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
-AppOpenAd? openAd;
-
-Future<void> loadAd() async {
-  await AppOpenAd.load(
-      adUnitId: 'ca-app-pub-3940256099942544/3419835294',
-      request: const AdRequest(),
-      adLoadCallback: AppOpenAdLoadCallback(onAdLoaded: (ad) {
-        print('ad is loaded');
-        openAd = ad;
-        openAd!.show();
-      }, onAdFailedToLoad: (error) {
-        print("ad failed to load $error");
-      }),
-      orientation: AppOpenAd.orientationPortrait);
-}
-
-void showAd() {
-  if (openAd == null) {
-    print("try to show before loading ");
-    loadAd();
-    return;
-  }
-  openAd!.fullScreenContentCallback = FullScreenContentCallback(
-    onAdShowedFullScreenContent: (ad) {
-      print("onAdShowedFullScreenContent");
-    },
-    onAdFailedToShowFullScreenContent: (ad, error) {
-      ad.dispose();
-      print('failed to load $error');
-      openAd = null;
-      loadAd();
-    },
-    onAdDismissedFullScreenContent: (ad) {
-      ad.dispose();
-      print("dismissed");
-      openAd = null;
-      loadAd();
-    },
-  );
-  openAd!.show();
-}
+import 'core/functions/ad_functions.dart';
 
 void main() async {
+  // Future.delayed(const Duration(seconds: 50), () {
+  //   print("Executed after 10 seconds");
+  // });
+
   WidgetsFlutterBinding.ensureInitialized();
   await MobileAds.instance.initialize();
-  await loadAd();
+  // ---------------------------------------------
+  print('Started at ${DateTime.now()}');
+  final time = await Future.delayed(
+    const Duration(seconds: 10),
+    () async {},
+  ).then((value) => DateTime.now());
+  print('Awaited time was at $time');
+  // --------------------------------------
+
+  await AdFunctions.loadAd();
   runApp(MyApp());
 }
 
